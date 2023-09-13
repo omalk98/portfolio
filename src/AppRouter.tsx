8afc4061292fbcdd20a0e6fa113cdf0e19e2499f
Omar Hussein <omarmohammad1951@gmail.com>
2023-09-13 16:18:22 -0400

    upgraded react router routes

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,35 +1,41 @@
-import { Suspense, lazy } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Loader from './components/loader';
-const Home = lazy(() => import('./pages/home'));
-const Blog = lazy(() => import('./pages/blog'));
-const Projects = lazy(() => import('./pages/projects'));
-const BlogPost = lazy(() => import('./pages/blog-post'));
+import { Suspense, lazy } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Loader from "./components/loader";
+const Home = lazy(() => import("./pages/home"));
+const Blog = lazy(() => import("./pages/blog"));
+const Projects = lazy(() => import("./pages/projects"));
+const BlogPost = lazy(() => import("./pages/blog-post"));
 
 export default function AppRouter() {
   return (
     <Suspense fallback={<Loader />}>
       <Routes>
-        <Route
-          path="/portfolio"
-          element={<Navigate to="/portfolio/home" />}
-        />
-        <Route
-          path="/portfolio/home"
-          element={<Home />}
-        />
-        <Route
-          path="/portfolio/blog"
-          element={<Blog />}
-        />
-        <Route
-          path="/portfolio/blog/:id"
-          element={<BlogPost />}
-        />
-        <Route
-          path="/portfolio/projects"
-          element={<Projects />}
-        />
+        <Route path='/portfolio'>
+          <Route
+            index
+            element={<Navigate to='/portfolio/home' />}
+          />
+
+          <Route
+            path='home'
+            element={<Home />}
+          />
+          <Route path='blog'>
+            <Route
+              index
+              element={<Blog />}
+            />
+
+            <Route
+              path=':id'
+              element={<BlogPost />}
+            />
+          </Route>
+          <Route
+            path='projects'
+            element={<Projects />}
+          />
+        </Route>
       </Routes>
     </Suspense>
   );
